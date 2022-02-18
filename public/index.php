0973562faf78561659ff5f7fca6febc775355bd5
jijihohococo <jijihohococo@gmail.com>
2022-02-18 16:34:31 +0630

    add

--- public/index.php
+++ public/index.php
@@ -7,6 +7,12 @@ use JiJiHoHoCoCo\PHPENV\ENV;
 
 use App\Kernel;
 
+if(!file_exists(__DIR__.'/../.env')){
+	
+	throw new Exception("Please create .env file under your project folder", 1);
+	
+}
+
 ENV::set(__DIR__.'/../.env');
 
 session_start();
