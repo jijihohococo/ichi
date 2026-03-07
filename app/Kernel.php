ee71400203e526912d2e1ebe221244a56635d50a
jijihohococo <jijihohococo@gmail.com>
2026-03-08 03:40:21 +0900

    fix: update dependencies in composer.json and refactor Kernel class

--- app/Kernel.php
+++ app/Kernel.php
@@ -10,12 +10,8 @@ use JiJiHoHoCoCo\IchiTemplate\Template\View;
 require_once __DIR__ . '/../routes/web.php';
 require_once __DIR__ . '/../routes/api.php';
 
-
-class Kernel{
-
-	public $autoloadMiddlewares = [
-		'JiJiHoHoCoCo\IchiRoute\Middleware\CSRFMiddleware'
-	];
+class Kernel
+{
 
 	public function run(){
 
@@ -30,13 +26,14 @@ class Kernel{
 
 		View::setPath(__DIR__.'/../resources/views/');
 
-		$route = new Route;
-		$route->setKeyValue("mysql", $connector->getConnection());
-		$route->setBaseControllerPath('App\Controllers');
-		$route->setBaseMiddlewarePath('App\Middlewares');
-		$route->setDefaultMiddlewares($this->autoloadMiddlewares);
-		web($route);
-		api($route);
-		$route->run();
-	}
+        $route = new Route();
+        $route->setKeyValue('mysql', $connector->getConnection());
+        $route->setBaseControllerPath('App\Controllers');
+        $route->setBaseMiddlewarePath('App\Middlewares');
+
+        web($route);
+        api($route);
+
+        $route->run();
+    }
 }
